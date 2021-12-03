cc05415b59ba8f57ddb2c268146cf0fae89ea119
Matis Rammi <matis.rammi@khk.ee>
2021-12-03 11:25:46 +0200

    Change city weather data

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const weather = new Weather("Tallinn")
 // UI object
 const ui = new UI();
 
-//Get city weather
+// Events
+const form = document.querySelector("#change-city")
+form.addEventListener("submit", changeCityWeather)
+
+// Change city weather
+function changeCityWeather(event) {
+    const city = document.querySelector("#city-name").value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector("#city-name").value = ""
+    event.preventDefault()
+}
+
+// Get city weather
 function getWeather() {
     weather.getWeather()
         .then(data => {
